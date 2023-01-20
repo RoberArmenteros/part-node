cc71e16d84d2df788743283cc2dc048fa6e2a5c8
RoberArmenteros <119441975+RoberArmenteros@users.noreply.github.com>
2023-01-20 18:19:41 +0100

    sdf

--- index.js
+++ index.js
@@ -95,7 +95,8 @@ app.use((request,request)=>{
   })
 })
 
-const PORT = 3001
+//const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT,() =>{
   console.log(`Server running on port ${PORT}`)
